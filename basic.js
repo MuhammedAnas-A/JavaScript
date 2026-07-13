ea94aa7fd0d0aa694e99c96b473342691187ba67
Muhammed Anas A <anasmuhammed5954@gmail.com>
2026-07-13 13:16:21 +0530

    first example

--- basic.js
+++ basic.js
@@ -1,6 +1,15 @@
-let a=10
-let b=20
-let temp=a
-a=b
-b=temp
-console.log("value of a: ",a,"value of b: ",b)
+// let a=10
+// let b=20
+// let temp=a
+// a=b
+// b=temp
+// console.log("value of a: ",a,"value of b: ",b)
+
+
+let name=prompt("Enter your Name: ")
+let age=prompt("Enter your Age: ")
+let email=prompt("Enter your Email: ")
+
+console.log("Name is: ",name)
+console.log("Age is: ",age)
+console.log("Email is: ",email)
