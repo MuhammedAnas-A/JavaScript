2a85f03072119f006fa1edb98fa5d4e6f4ea865e
Muhammed Anas A <anasmuhammed5954@gmail.com>
2026-07-13 23:51:11 +0530

    fuel problem added

---
+++ fuel.js
@@ -0,0 +1,9 @@
+let dist=prompt("Enter distance to travel(kms):")
+let mileage=prompt("Enter mileage of bike(km/l):")
+let priceOfPetrol=100
+fuel=dist/mileage
+totalCost=fuel*priceOfPetrol
+
+alert("Distance is: kms"+dist)
+alert("Total fuel cost is: ₹"+totalCost)
+
