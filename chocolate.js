1bfb7a1dbcb6dece71c90ccd3c6e85fd3bab1ba5
Muhammed Anas A <anasmuhammed5954@gmail.com>
2026-07-14 23:33:19 +0530

    chocolate problem added

---
+++ chocolate.js
@@ -0,0 +1,7 @@
+let chocolates=prompt("Enter total no.of chocolates: ")
+let hold=12
+let nOfboxes=Math.floor(chocolates/hold)
+let remChocolate=chocolates%hold
+
+alert("Number of boxes: "+nOfboxes)
+alert("Remaining chocolates: "+remChocolate)
