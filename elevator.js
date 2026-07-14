2e4eb6593c0dc80d46dd2a6714edebc4703824c1
Muhammed Anas A <anasmuhammed5954@gmail.com>
2026-07-15 00:01:56 +0530

    elevator problem added

---
+++ elevator.js
@@ -0,0 +1,7 @@
+let people=prompt("Enter total no.of people: ")
+let eachtrip=8
+let nOfTrip=Math.floor(people/eachtrip)
+let remPeople=people%eachtrip
+
+alert("Number of Trips: "+nOfTrip)
+alert("Remaining peoples: "+remPeople)
