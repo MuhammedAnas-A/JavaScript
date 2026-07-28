cf0a2785311e50661d6434b38d0c06b8bf4ec172
Muhammed Anas A <anasmuhammed5954@gmail.com>
2026-07-28 16:41:25 +0530

    internet connection problem added

---
+++ internet.js
@@ -0,0 +1,25 @@
+let input = [1, 1, 1, 0, 1, 0, 1, 1]
+let total = 0
+let curr = 0
+let prev = 0
+let index = 0
+
+for(let i=0;i<input.length;i++){
+    if(input[i] == 1){
+        curr++
+        if(curr == 2){
+            total++
+        }
+            if(curr > prev){
+                prev = curr
+                index = i - curr + 1
+            }
+    }
+    else{
+        curr = 0
+    }
+}
+
+console.log("Longest Stable Connection: "+prev)
+console.log("Longest Streak Starts At: Index "+index)
+console.log("Total Stable Connection Periods: "+total)
