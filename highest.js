5bb62db2fc92dd8f4855a7f00447dac008c68ac7
Muhammed Anas A <anasmuhammed5954@gmail.com>
2026-07-29 22:25:07 +0530

    highest element problem added

---
+++ highest.js
@@ -0,0 +1,18 @@
+let array = [10, 2, 3, 8, 4, 5, 20]
+let high = 0
+let secHigh = 0
+
+for(let i=0;i<array.length;i++){
+    if(array[i] > high){
+        secHigh = high
+        high = array[i]
+        
+    }
+    if(array[i] > secHigh && array[i] < high){
+        secHigh = array[i]
+    }
+}
+
+console.log("Highest Element: "+high)
+console.log("Second Highest Element: "+secHigh)
+
