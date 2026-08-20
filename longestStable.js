7d56a97356df319d18ebb5c72446d5e513960cdf
Muhammed Anas A <anasmuhammed5954@gmail.com>
2026-08-20 11:55:46 +0530

    longest stable added

--- longestStable.js
+++ longestStable.js
@@ -1,12 +1,12 @@
 // let arr = [1,2,3,2,1,5]
-let arr = [5,4,3,2,8,9]
+// let arr = [5,4,3,2,8,9]
 // let arr = [1,2,2,3,4,3,2]
-// let arr = [1,2,3,2,3,4,5,1]
+let arr = [1,2,3,2,3,4,5,1]
 function longestStableRun(arr){
 
-    let count = 0
-    let decreasing = 0
-    let increasing = 0
+    let count = 1
+    let decreasing = 1
+    let increasing = 1
     
     for(let i=0;i<arr.length - 1;i++){
         
@@ -14,22 +14,32 @@ function longestStableRun(arr){
             
                 
                 increasing++
-                count = increasing
+                decreasing = 1
+                if(increasing > count){
+                        count = increasing
+                }
             
         }
         
         
-        if(arr[i] > arr[i+1]){
+        else if(arr[i] > arr[i+1]){
             
                 
                 decreasing++
-                count = decreasing
+                increasing = 1
+                if(decreasing > count){
+                        count = decreasing
+                }
             
             
         }
+        else{
+                increasing = 1
+                decreasing = 1
+        }
         
-     return count   
-    }
+}
+  return count   
 }
 
 console.log(longestStableRun(arr))
